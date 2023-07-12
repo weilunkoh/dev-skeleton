f39f14392971d0b4a364e9d7c8cdef4ee7e83257
Wei Lun Koh <wei_lun_koh@aiap.sg>
2023-07-12 14:14:35 +0800

    added more description to dummy.py

--- src/dummy.py
+++ src/dummy.py
@@ -1,3 +1,4 @@
+# iSort will sort the below imports into alphbetical order
 import hahaha
 import lolol
 
@@ -6,6 +7,7 @@ def short_function_name():
     pass
 
 
+# Black will format the below function to be the format below
 # def long_function_name(param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, param12):
 #     pass
 
