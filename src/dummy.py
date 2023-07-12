71b8d3d74380bd69ce025c1a92da1ce111708528
Wei Lun Koh <wei_lun_koh@aiap.sg>
2023-07-12 13:57:12 +0800

    initial commit

---
+++ src/dummy.py
@@ -0,0 +1,27 @@
+import hahaha
+import lolol
+
+
+def short_function_name():
+    pass
+
+
+# def long_function_name(param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, param12):
+#     pass
+
+
+def long_function_name(
+    param1,
+    param2,
+    param3,
+    param4,
+    param5,
+    param6,
+    param7,
+    param8,
+    param9,
+    param10,
+    param11,
+    param12,
+):
+    pass
